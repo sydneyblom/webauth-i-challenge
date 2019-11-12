7390490af344aa7f3f2084fb4ea70035ae3a36aa
Sydney Blom <sydneyblom@#gmail.com>
2019-11-12 17:33:12 -0600

    MVP

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,7 +4,8 @@ const router = require('express').Router();
 
 const Users = require('../users/users-model');
 
-const reqauth = require('../auth/requires-auth-middleware')
+
+
 router.post('/register', (req, res) => {
   let userInformation = req.body;
 
@@ -13,6 +14,7 @@ router.post('/register', (req, res) => {
 
     Users.add(userInformation)
       .then(saved => {
+        req.session.username = saved.username;
         res.status(201).json(saved);
       })
       .catch(error => {
@@ -21,14 +23,14 @@ router.post('/register', (req, res) => {
   });
 });
 
-router.post('/login', reqauth, (req, res) => {
+router.post('/login', (req, res) => {
   let { username, password } = req.body;
-
   Users.findBy({ username })
     .first()
     .then(user => {
       // check that the password is valid
       if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.username = user.username; //creates the cookie and sends it back to client//clients holds onto that cookie
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -40,4 +42,18 @@ router.post('/login', reqauth, (req, res) => {
     });
 });
 
+
+router.get('/logout', (req,res) => {
+    if(req.session) {
+        req.session.destroy(err =>{
+            if (err) {
+              res.status(500).json({message: 'Error logging out'})
+            } else {
+                res.status(20).end();
+            }
+        })
+    } else {
+        res.status(200).json({message: "You are logged out"})
+    }
+})
 module.exports = router;
--- auth/requires-auth-middleware.js
+++ auth/requires-auth-middleware.js
@@ -1,28 +1,8 @@
-const bcrypt = require('bcryptjs');
 
-const Users = require('../users/users-model');
-
-module.exports = (req, res, next) => {
-  let { username, password } = req.headers;
-  //since get request need to read from the headers 
-//   let { username, password } = req.body;
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next(); 
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        console.log('login error', error);
-        res
-          .status(500)
-          .json({ message: 'Ran into an error, please try later' });
-      });
+module.exports = (req, res, next) => { 
+  if (req.session && req.session.username) {
+    next();
   } else {
-    res.status(400).json({ message: 'Please provide credentials' });
+    res.status(401).json({ message: 'Not authorized' });
   }
 };
